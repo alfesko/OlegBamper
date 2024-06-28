4aa9d5a12eafef42bd7d5580ac58e8fd600af8a9
fesko <fesko_av_21@mf.grsu.by>
2024-06-28 12:23:53 +0300

    db changelog

---
+++ db-changelog/db-changelog-1.0.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
+CREATE TABLE "session" (
+    "sid" varchar NOT NULL COLLATE "default",
+    "sess" json NOT NULL,
+    "expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid");
+
+CREATE INDEX "IDX_session_expire" ON "session" ("expire");
