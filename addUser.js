0ee3f5c75a5ec1f11bf8ef8f5e5335432df5c1e8
fesko <fesko_av_21@mf.grsu.by>
2024-06-28 17:34:37 +0300

    mixed up the files

--- addUser.js
+++ addUser.js
@@ -1,101 +1,21 @@
-const express = require('express');
-const session = require('express-session');
-const path = require('path');
 const bcrypt = require('bcrypt');
 const { Pool } = require('pg');
-const pgSession = require('connect-pg-simple')(session);
 const dbConfig = require('./db-config');
-
-const app = express();
-const PORT = process.env.PORT || 3000;
 
 const pool = new Pool(dbConfig);
 
-app.use(session({
-    store: new pgSession({
-        pool: pool,
-        tableName: 'session'
-    }),
-    secret: 'your_secret_key',
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: false }
-}));
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-async function hashPassword(password) {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    return hashedPassword;
-}
-
 async function addUser(username, password) {
     try {
-        const hashedPassword = await hashPassword(password);
+        const hashedPassword = await bcrypt.hash(password, 10);
+
         const client = await pool.connect();
-        await client.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, hashedPassword]);
+        const result = await client.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, hashedPassword]);
         client.release();
-        console.log(`Пользователь с логином '${username}' успешно добавлен в базу данных!`);
+
+        console.log(`Пользователь с логином ${username} успешно добавлен в базу данных!`);
     } catch (err) {
         console.error('Ошибка при добавлении пользователя:', err);
     }
 }
 
-app.post('/register', (req, res) => {
-    const { username, password } = req.body;
-    addUser(username, password)
-        .then(() => {
-            res.send('Пользователь успешно зарегистрирован!');
-        })
-        .catch(err => {
-            res.status(500).send('Ошибка при регистрации пользователя');
-        });
-});
-
-app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-    try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM users WHERE username = $1', [username]);
-        client.release();
-        if (result.rows.length > 0) {
-            const user = result.rows[0];
-            const isPasswordMatch = await bcrypt.compare(password, user.password);
-            if (isPasswordMatch) {
-                req.session.loggedIn = true;
-                res.redirect('/');
-            } else {
-                res.send('Неверные учетные данные');
-            }
-        } else {
-            res.send('Пользователь не найден');
-        }
-    } catch (err) {
-        console.error('Ошибка при входе:', err);
-        res.status(500).send('Ошибка при входе пользователя');
-    }
-});
-
-app.get('/logout', (req, res) => {
-    req.session.destroy(err => {
-        if (err) {
-            console.error('Ошибка при выходе:', err);
-            return res.status(500).send('Ошибка при выходе');
-        }
-        res.clearCookie('connect.sid');
-        res.redirect('/');
-    });
-});
-
-app.get('/', (req, res) => {
-    if (req.session.loggedIn) {
-        res.sendFile(path.join(__dirname, 'protected', 'main.html'));
-    } else {
-        res.sendFile(path.join(__dirname, 'login.html'));
-    }
-});
-
-app.listen(PORT, () => {
-    console.log(`Сервер запущен на порту ${PORT}`);
-});
+addUser('oleg2', 'oleg227062024');
